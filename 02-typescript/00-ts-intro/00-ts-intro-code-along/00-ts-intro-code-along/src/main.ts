30956932c633a21934d34245ba937c2424f66c1a
rosaTome <tomerosalinda@gmail.com>
2024-04-04 08:36:22 +0100

    code alongs and challenges commented out to git push work

--- 02-typescript/00-ts-intro/00-ts-intro-code-along/00-ts-intro-code-along/src/main.ts
+++ 02-typescript/00-ts-intro/00-ts-intro-code-along/00-ts-intro-code-along/src/main.ts
@@ -1,33 +1,33 @@
-import "./style.scss";
+// import "./style.scss";
 
-// inferred types - not specifically saying what type this is 
-const firstName = "Ollie"
-firstName.length
+// // inferred types - not specifically saying what type this is 
+// const firstName = "Ollie"
+// firstName.length
 
-// firstName * 3 // ERROR
-let variableToBeDefined; // any inferred as any :(
+// // firstName * 3 // ERROR
+// let variableToBeDefined; // any inferred as any :(
 
-// explicit types
-const lastName: string = "Robins"
-const age: number = 30
+// // explicit types
+// const lastName: string = "Robins"
+// const age: number = 30
 
-console.log(`Hello my name is ${firstName} ${lastName}, my age is ${age}`);
+// console.log(`Hello my name is ${firstName} ${lastName}, my age is ${age}`);
 
-const isHungry: boolean = true
+// const isHungry: boolean = true
 
-if (isHungry) {
-  console.log("go eat!!")
-}
+// if (isHungry) {
+//   console.log("go eat!!")
+// }
 
-// arrays 
-const names : string = ["Molly", "Lidia", "Josh"];
+// // arrays 
+// const names : string = ["Molly", "Lidia", "Josh"];
 
-//create a loop that loops through the names array and gets the length of each name and pushes it into a new array
+// //create a loop that loops through the names array and gets the length of each name and pushes it into a new array
 
-const lengths : number[] = [];
+// const lengths : number[] = [];
 
-for (let i: numnber = 0; i < names.length; i++) {
-    lengths.push(names[i].length);
-}
+// for (let i: numnber = 0; i < names.length; i++) {
+//     lengths.push(names[i].length);
+// }
 
-console.log(lengths);
+// console.log(lengths);
--- 02-typescript/03-complex-data/02-complex-data-challenge-02/src/main.ts
+++ 02-typescript/03-complex-data/02-complex-data-challenge-02/src/main.ts
@@ -1,40 +1,40 @@
-import { companies } from "./data";
+// import { companies } from "./data";
 
-// Create a function that takes a string of name and checks how much good is the given name. A preloaded dictionary of alphabet scores is available in the Code tab. Add up the letters of your name to get the total score.
+// // Create a function that takes a string of name and checks how much good is the given name. A preloaded dictionary of alphabet scores is available in the Code tab. Add up the letters of your name to get the total score.
 
-const scores = {"A": 100, "B": 14, "C": 9, "D": 28, "E": 145, "F": 12, "G": 3,
-"H": 10, "I": 200, "J": 100, "K": 114, "L": 100, "M": 25,
-"N": 450, "O": 80, "P": 2, "Q": 12, "R": 400, "S": 113,
-"T": 405, "U": 11, "V": 10, "W": 10, "X": 3, "Y": 210, "Z": 23}
+// const scores = {"A": 100, "B": 14, "C": 9, "D": 28, "E": 145, "F": 12, "G": 3,
+// "H": 10, "I": 200, "J": 100, "K": 114, "L": 100, "M": 25,
+// "N": 450, "O": 80, "P": 2, "Q": 12, "R": 400, "S": 113,
+// "T": 405, "U": 11, "V": 10, "W": 10, "X": 3, "Y": 210, "Z": 23}
 
-// Return your result as per the following rules:
+// // Return your result as per the following rules:
 
-//   score <= 60:   "NOT TOO GOOD"
-//   61 <= score <= 300:  "PRETTY GOOD"
-//   301 <= score <= 599:  "VERY GOOD"
-//   score >= 600:  "THE BEST"
-// getNameScore("MUBASHIR") ➞ "THE BEST" getNameScore("YOU") ➞ "VERY GOOD" getNameScore("MATT") ➞ "THE BEST" getNameScore("PUBG") ➞ "NOT TOO GOOD"
+// //   score <= 60:   "NOT TOO GOOD"
+// //   61 <= score <= 300:  "PRETTY GOOD"
+// //   301 <= score <= 599:  "VERY GOOD"
+// //   score >= 600:  "THE BEST"
+// // getNameScore("MUBASHIR") ➞ "THE BEST" getNameScore("YOU") ➞ "VERY GOOD" getNameScore("MATT") ➞ "THE BEST" getNameScore("PUBG") ➞ "NOT TOO GOOD"
 
-// loop
-function getNameScore(name: string): string {
-  let totalScore: number = 0;
-  for (let i = 0; i < name.length; i++) {
-    const letter: string = name[i].toUpperCase();
-    if (scores.hasOwnProperty(letter)) {
-      totalScore += scores[letter];
-    }
-  }
+// // loop
+// function getNameScore(name: string): string {
+//   let totalScore: number = 0;
+//   for (let i = 0; i < name.length; i++) {
+//     const letter: string = name[i].toUpperCase();
+//     if (scores.hasOwnProperty(letter)) {
+//       totalScore += scores[letter];
+//     }
+//   }
   
-  // if 
-}
-  if (totalScore <= 60) {
-    return "NOT TOO GOOD";
-  } else if (totalScore <= 300) {
-    return "PRETTY GOOD";
-  } else if (totalScore <= 599) {
-    return "VERY GOOD";
-  } else {
-    return "THE BEST";
-  }
-}
+//   // if 
+// }
+//   if (totalScore <= 60) {
+//     return "NOT TOO GOOD";
+//   } else if (totalScore <= 300) {
+//     return "PRETTY GOOD";
+//   } else if (totalScore <= 599) {
+//     return "VERY GOOD";
+//   } else {
+//     return "THE BEST";
+//   }
+// }
 
