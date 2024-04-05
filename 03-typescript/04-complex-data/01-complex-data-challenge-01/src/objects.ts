94fac55828c220c7b6c7e683dd1d327576cf460f
rosaTome <tomerosalinda@gmail.com>
2024-04-05 17:43:38 +0100

    resolving conflicts

---
+++ 03-typescript/04-complex-data/01-complex-data-challenge-01/src/objects.ts
@@ -0,0 +1,44 @@
+// objects 
+
+//type dog
+
+type Dog = {
+    name: string;
+    breed: string;
+    "favourite food": string;
+    age: number;
+    legs: number;
+    color: string;
+    trainer: boolean;
+    "favourite toy"?: string; 
+
+}
+
+// objects based on the dog type 
+const dogOne: Dog = {
+    name: "Toto",
+    breed: "Goldendoodle",
+    "favourite food" : "chicken",
+    age: 1.0,
+    legs: 4,
+    color: "golden",
+    trained: false,
+}
+
+const dogTwo : Dog = {
+    name: "pip",
+    breed: "spaniel",
+    "favourite food": "carrots",
+    age: 14, 
+    legs: 4, 
+    color: "black, brown",
+    trained: true,
+    "favourite toy": "dinosaur",
+}
+
+// access dogOne's, breed, trained, favourite food 
+console.log(dogOne.breed);
+console.log(dogOne.trained);
+console.log(dogOne["favourite food"]);
+
+
