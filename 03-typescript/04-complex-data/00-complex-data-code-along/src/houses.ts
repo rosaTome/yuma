f1f27000228ced0a6d016086121775b37219a764
rosaTome <tomerosalinda@gmail.com>
2024-04-06 01:14:21 +0100

    complex data houses.ts code along reviewed

--- 03-typescript/04-complex-data/00-complex-data-code-along/src/houses.ts
+++ 03-typescript/04-complex-data/00-complex-data-code-along/src/houses.ts
@@ -3,55 +3,56 @@
 type House = {
     rooms: Room[];
     address: Address;
-  };
+};
   
-  type Room = {
-    name: string;
-    hasFirePlace: boolean;
-  };
-  
-  type Address = {
-    houseName: string;
-    street: string;
-    town: string;
-    region: string;
-  };
-  
-  const myHouse : House = {
-    rooms: [
-      {name: "front room", hasFirePlace: false},
-      {name: "bathroom", hasFirePlace: false},
-      {name: "kitchen", hasFirePlace: false},
-    ],
-    address: {
-      houseName : "beg end",
-      street: "bagshot row",
-      town: "hobbiton",
-      region: "the shire",
-    },
-  };
-  
-  // HOUSE COMPLEX DATA - CHALLENGES:
+type Room = {
+  name: string;
+  hasFirePlace: boolean;
+};
+
+type Address = {
+  houseName: string;
+  street: string;
+  town: string;
+  region: string;
+};
+
+const myHouse : House = {
+  rooms: [
+    {name: "front room", hasFirePlace: false},
+    {name: "bathroom", hasFirePlace: false},
+    {name: "kitchen", hasFirePlace: false},
+  ],
+  address: {
+    houseName : "beg end",
+    street: "bagshot row",
+    town: "hobbiton",
+    region: "the shire",
+  },
+};
+
+// HOUSE COMPLEX DATA - CHALLENGES:
   
   // 1. HOW DO YOU GET ACCESS TO THE HOUSE NAME?
-  // for in?
-  const houseName = myHouse.address.houseName;
-  console.log("houseName");
+  console.log(myHouse.address.houseName);
   
   // 2. HOW DO YOU GET ACCESS TO THE REGION IN THE ADDRESS?
-  const region = myHouse.address.region;
-  console.log(region);
+  console.log(myHouse.address.region);
   
   // 3. HOW DO YOU GET ACCESS TO THE FIRST ROOM NAME OBJECT IN THE ROOMS ARRAY?
-  const firstRoomName = myHouse.rooms[0].name;
-  console.log(firstRoomName);
+  console.log(myHouse.rooms[0].name);
   
   // 4. HOW DO YOU GET ACCESS TO THE NAME OF THE LAST ROOM IN THE ROOMS ARRAY?
+  // console.log(myHouse.rooms[2].name);
   const lastRoomIndex = myHouse.rooms.length - 1;
   console.log(myHouse.rooms[lastRoomIndex].name);
   
   // 5. CAN YOU CREATE A NEW ARRAY WITH ONLY ROOMS WITH FIREPLACES?
-  //    - CREATE AN ARRAY OF [<li>Room name</li>, <li>Room name</li>]
-  const roomsWithFireplaces = myHouse.rooms.filter(room => room.hasFirePlace);
-  const roomNamesWithFireplaces = roomsWithFireplaces.map(room => `<li>${room.name}</li>`);
-  console.log(roomNamesWithFireplaces);
+    // CREATE AN ARRAY OF [<li>Room name</li>, <li>Room name</li>]
+  const roomsWithFireplaces = myHouse.rooms.filter((room) => room.hasFirePlace);
+
+  const roomLi = roomsWithFireplaces.map((room) => {
+    return `<li>${room.name}</li>`;
+  });
+
+  console.log(roomLi);
