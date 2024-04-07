9bfad0f5b748b9e93d8ec0af9e40267a0b53a368
rosaTome <tomerosalinda@gmail.com>
2024-04-07 18:41:58 +0100

    js5 arrays interators part 2 reviewed

--- 03-typescript/00-ts-intro/01-ts-intro-challenges/01-ts-intro-challenges/src/main.ts
+++ 03-typescript/00-ts-intro/01-ts-intro-challenges/01-ts-intro-challenges/src/main.ts
@@ -1,112 +1,111 @@
-// import "./style.scss";
+import "./style.scss";
 
-// // ---Challenge 1---
+// ---Challenge 1---
 
-// // Create a random number between 1 - 10 inclusive and assign it to a variable. ✅
-//   // Can you add an Explicit type declaration to this variable? ✅
+// Create a random number between 1 - 10 inclusive and assign it to a variable. ✅
+  // Can you add an Explicit type declaration to this variable? ✅
 
-// console.log("---Challenge 1---");
+console.log("---Challenge 1---");
 
-// const randomNumber: number = 10
+const randomNumber: number = 10
  
-// console.log(randomNumber);
+console.log(randomNumber);
 
-// // ---Challenge 2---
+// ---Challenge 2---
 
-// console.log("---Challenge 2---");
+console.log("---Challenge 2---");
 
-// // Create an array of 5 random numbers between 1 - 10 inclusive. ✅
-//   // Can you add an Explicit type declaration to the variable storing the array? ✅
-//   // Can you use a loop to populate the array with the random numbers? ✅
+// Create an array of 5 random numbers between 1 - 10 inclusive. ✅
+  // Can you add an Explicit type declaration to the variable storing the array? ✅
+  // Can you use a loop to populate the array with the random numbers? ✅
 
+const randomNumbers: number[] = [1, 3, 5, 7, 9];
 
-// const randomNumbers: number[] = [1, 3, 5, 7, 9];
+for (let i = 0; i < 5; i++) {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  randomNumbers.push(randomNumber);
+}
 
-// for (let i = 0; i < 5; i++) {
-//   const randomNumber = Math.floor(Math.random() * 10) + 1;
-//   randomNumbers.push(randomNumber);
-// }
+console.log(randomNumbers);
 
-// console.log(randomNumbers);
+// FUNCTIONS & UNIONS 
 
-// // FUNCTIONS & UNIONS 
+// create a fucntion that takes a string and logs it to the console 
 
-// // create a fucntion that takes a string and logs it to the console 
+const greatUser = (firstName: string): void => {
+  console.log(`Hello ${firstName}`);
+}
 
-// const greatUser = (firstName: string): void => {
-//   console.log(`Hello ${firstName}`);
-// }
+greatUser("Toto");
 
-// greatUser("Toto");
+// create a function that takes multuple numbers and multiplies them 
 
-// // create a fucntion that takes multuple numbers and multiplies them 
+const multiplyNums = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
 
-// const multiplyNums = (num1: number, num2: number): number => {
-//   return num1 + num2;
-// };
+console.log(multiplyNums(5, 10));
 
-// console.log(multiplyNums(5, 10));
+// create a union type and use the prompt () to demonstrate 
 
-// // create a union type and use the prompt () to demonstrate 
+let union: string | number | null;
 
-// let union: string | number;
+union = 1
+union = "Ollie"
 
-// union = 1
-// union = "Ollie"
+const getUserNumberInput = () :number => {
 
-// // const getUserNumberInput = () :number => {
+  // user input will be stored inside the input variable
+  const input = prompt("Please enter a number between 1-10");  // input is either string or numm
 
-//   // user input will be stored inside the input variable
-//   const input = prompt("Please enter a numnber between 1-10");  // input is either string or numm
+  if (input === null) {
+    return -1;
+  }
 
-//   if (input === null) {
-//     return -1;
-//   }
+  // console.log(input);
 
-//   // console.log(input);
+  return Number(input);
+};
 
-//   return Number(input);
-// };
-
-// // getUserNumberInput()
-
-
-// // // Validate guess function
-
-// // const validateGuess =(
-// //     toCheck:number, 
-// //     target:number
-// //   ):  "equals" | "too high" | "too low" => {
-// // if (toCheck === target) {
-// //   return "equals";
-// //   } else if (toCheck > target) {
-// //     return "too high";
-// //   } else {
-// //     return "too low";
-// //   }
-// // };
-
-// // // put it all together
-// // // we need a user input - which will be the number to check
-// // const userInput = getUserNumberInput()
-// // console.log(userInput);
+getUserNumberInput()
 
 
-// // //  we need a result from the validate guess 
-// // const result = validateGuess(userInput, randomNumber)
+// Validate guess function
+const validateGuess =(
+    toCheck:number, 
+    target:number
+):  "equals" | "too high" | "too low" => {
+if (toCheck === target) {
+  return "equals";
+  } else if (toCheck > target) {
+    return "too high";
+  } else {
+    return "too low";
+  }
+};
 
-// // // alert the user with their guess 
+// put it all together
+// we need a user input - which will be the number to check
+const userInput = getUserNumberInput()
+console.log(userInput);
 
-// // alert(`Guess : ${userInput}, the result was : ${result}`)
 
-// // Functions & Unions Challenge 
+//  we need a result from the validate guess 
+const result = validateGuess(userInput, randomNumber)
 
-// console.log("Functions & Unions Challenge");
+// alert the user with their guess 
+
+alert(`Guess : ${userInput}, the result was : ${result}`)
+
+// Functions & Unions Challenge 
+
+console.log("Functions & Unions Challenge");
 
 // const validateGuess = (
 //   toCheck: number,
 //   target: number
 // ): "equals" | "too high" | "too low" | "out of range" => {
+
 //   if (toCheck < 1 || toCheck > 10) {
 //       return "out of range";
 //   } else if (toCheck === target) {
@@ -117,7 +116,3 @@
 //       return "too low";
 //   }
 // };
-
-
-
-
